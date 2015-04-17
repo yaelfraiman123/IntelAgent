4866bfa9e93a6fa7e2959824c369f093efcd3f64
tarieli1 <tarieli1@gmail.com>
2015-04-17 21:29:22 +0300

    tamir

--- Client/js/LoginController.js
+++ Client/js/LoginController.js
@@ -5,7 +5,7 @@
   var LoginController = function($scope,$location,$log) {
 
     $scope.login = function(username,password) {
-      console.log("username: " + username + " Password: " +password);
+      console.log("username: " + username + " Password: " + password);
       $log.debug("Going to the Action Page via Login");
       $location.path("/action");
     };
