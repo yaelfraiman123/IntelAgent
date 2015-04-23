676063bf9dafd07dfdbaa260ff0559d8bdc0f075
yaelfraiman123 <yaelfraiman@gmail.com>
2015-04-23 19:25:38 +0300

    unwanted changes

--- Client/js/LoginController.js
+++ Client/js/LoginController.js
@@ -2,15 +2,38 @@
 
   var app = angular.module("intelAgent");
 
-  var LoginController = function($scope,$location,$log) {
+  var LoginController = function($scope,$location,$log,userAccount,currentUser) {
 
-    $scope.login = function(username,password) {
-      console.log("username: " + username + " Password: " + password);
-      $log.debug("Going to the Action Page via Login");
-      $location.path("/action");
-    };
-  };
+		$scope.login = function(){
+			$scope.userData.grant_type = "password";
 
-  app.controller("LoginController", ["$scope","$location","$log",LoginController]);
+			userAccount.login.loginUser($scope.userData,
+				function(data){//on Success
+					$scope.message = "";
+					$scope.password = "";
+					
+					currentUser.setProfile($scope.userData.username,data.access_token,true);//init current user profile
+					$log.debug("Going to the Action Page via Login");
+					$location.path("/action");
+				},
+				function(response){//on Failure
+					$scope.password ="";
+					currentUser.setProfile("","",false);//reset current user
+					$scope.message = response.statusText + "/r/n";
+					if(response.data.exceptionMessage)
+						$scope.message += response.data.exceptionMessage + "/r/n" ;
+					if(response.data.error)
+						$scope.message += response.data.error + "/r/n";
+				});
+		};
+		
+		$scope.goToRegisterPage = function goToRegisterPage(){
+			$log.debug("Going to the Register Page via Login");
+			$location.path("/register");
+		};
+	};
+
+
+  app.controller("LoginController", ["$scope","$location","$log",'userAccount','currentUser',LoginController]);
 
 }()); 
