ace961b6d3117309d718d2c5c31cfd29413e58d6
yaelfraiman123 <yaelfraiman@gmail.com>
2015-07-24 08:18:03 +0300

    db handler change

--- IntelAgentWebApi/IntelAgentWebApi/common/DBHandler.cs
+++ IntelAgentWebApi/IntelAgentWebApi/common/DBHandler.cs
@@ -10,7 +10,6 @@ namespace IntelAgentWebApi.common
     public class DBHandler
     {
         private static DBHandler  _instance = new DBHandler();
-        private Entities _context = new Entities();
         private DBHandler()
         {
             
@@ -23,63 +22,78 @@ namespace IntelAgentWebApi.common
 
         public void UpdateActionLog(stocks_action i_Sell, stocks_action i_Buy, int i_Quantity, float i_Average)
         {
-            _context.action_log.Add(new action_log()
+            using (Entities _context = new Entities())
             {
-                sell_user_id = i_Sell.user_id,
-                buy_user_id = i_Buy.user_id,
-                quantity_ = i_Quantity,
-                price_ = i_Average,
-                stock_name_ = i_Sell.stock_name,
-                date_time = DateTime.Now,
-                Id = Guid.NewGuid().ToString()
-            });
-            _context.SaveChanges();
+                _context.action_log.Add(new action_log()
+                {
+                    sell_user_id = i_Sell.user_id,
+                    buy_user_id = i_Buy.user_id,
+                    quantity_ = i_Quantity,
+                    price_ = i_Average,
+                    stock_name_ = i_Sell.stock_name,
+                    date_time = DateTime.Now,
+                    Id = Guid.NewGuid().ToString()
+                });
+                _context.SaveChanges();
+                
+            }
+            
         }
 
         public void AddNewStockAction(stocks_action i_StocksDataManager)
         {
-            i_StocksDataManager.status = StockStatusGetter.GetDescription(eStatus.InProgress);
-            _context.stocks_action.Add(i_StocksDataManager);
-            _context.SaveChanges();
+            using (Entities _context = new Entities())
+            {
+                i_StocksDataManager.status = StockStatusGetter.GetDescription(eStatus.InProgress);
+                _context.stocks_action.Add(i_StocksDataManager);
+                _context.SaveChanges();
+            }
         }
 
         public List<stocks_action> GetStocksByUserId(string i_UserId)
         {
-            var stockslst = _context.stocks_action.ToList();
-            return stockslst.Where(x => x.user_id == i_UserId).ToList();
+            using (Entities _context = new Entities())
+            {
+                var stockslst = _context.stocks_action.ToList();
+                return stockslst.Where(x => x.user_id == i_UserId).ToList();
+            }
         }
 
         public stocks_action UpdateActionStock(stocks_action i_Stock)
         {
-
-            var stockToUpdate = _context.stocks_action.FirstOrDefault(x => x.Id == i_Stock.Id);
-            if (stockToUpdate != null)
+            using (Entities _context = new Entities())
             {
-                i_Stock.Id = stockToUpdate.Id;
-                i_Stock.date_time = stockToUpdate.date_time;
-                i_Stock.user_id = stockToUpdate.user_id;
-                stockToUpdate = i_Stock;
-                _context.SaveChanges();
+                var stockToUpdate = _context.stocks_action.FirstOrDefault(x => x.Id == i_Stock.Id);
+                if (stockToUpdate != null)
+                {
+                    i_Stock.Id = stockToUpdate.Id;
+                    i_Stock.date_time = stockToUpdate.date_time;
+                    i_Stock.user_id = stockToUpdate.user_id;
+                    stockToUpdate = i_Stock;
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("the stock not found in the db");
+                }
+                return stockToUpdate;
             }
-            else
-            {
-                throw new Exception("the stock not found in the db");
-            }
-            return stockToUpdate;
         }
 
         public stocks_action DeleteStockAction(string i_Id)
         {
-
-            var stockToDelete = _context.stocks_action.FirstOrDefault(x => x.Id == i_Id);
-            if (stockToDelete == null)
+            using (Entities _context = new Entities())
             {
-                throw new Exception("Stock Action not found");
+                var stockToDelete = _context.stocks_action.FirstOrDefault(x => x.Id == i_Id);
+                if (stockToDelete == null)
+                {
+                    throw new Exception("Stock Action not found");
+                }
+                stockToDelete.status = StockStatusGetter.GetDescription(eStatus.Deleted);
+                stockToDelete.is_updatable = 0;
+                _context.SaveChanges();
+                return stockToDelete;
             }
-            stockToDelete.status = StockStatusGetter.GetDescription(eStatus.Deleted);
-            stockToDelete.is_updatable = 0;
-            _context.SaveChanges();
-            return stockToDelete;
         }
     }
 }
